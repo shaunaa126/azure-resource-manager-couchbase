660d13547dd55cbd433095ac7f33d92dbf5fdfd8
Shaun Alexander <shaun.alexander@levvel.io>
2017-06-27 14:05:26 -0500

    Added region as a parameter

--- simple/deploy.sh
+++ simple/deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 RESOURCE_GROUP=$1
+REGION=$2
 
 # Azure CLI 1.0 commands
-azure group create $RESOURCE_GROUP westus
+azure group create $RESOURCE_GROUP $REGION
 azure group deployment create --template-uri https://raw.githubusercontent.com/shaunaa126/azure-resource-manager-couchbase/master/simple/mainTemplate.json --parameters-file mainTemplateParameters.json $RESOURCE_GROUP couchbase
 
 # Azure CLI 2.0 commands
-#az group create --name $RESOURCE_GROUP --location westus
+#az group create --name $RESOURCE_GROUP --location $REGION
 #az group deployment create --template-uri https://raw.githubusercontent.com/shaunaa126/azure-resource-manager-couchbase/master/simple/mainTemplate.json --parameters @mainTemplateParameters.json --resource-group $RESOURCE_GROUP
